550527f35b2cb66fa395ca18c264d5b571fad405
Administrator <2780837020@qq.com>
2017-03-06 14:54:18 +0800

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/3/6.
+ */
+
+/**
+ *
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello,java.");
+    }
+}
